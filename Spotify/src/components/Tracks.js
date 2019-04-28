9649ceb2248c52039cd08467a1d9e484ca32204c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-28 21:14:00 +0300

    play audio

--- Spotify/src/components/Tracks.js
+++ Spotify/src/components/Tracks.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
 
 class Tracks extends Component {
+    state = { playing: false, audio: null, playingPreviewUrl: null }
+
+    playAudio = previewUrl => () => {
+        const audio = new Audio(previewUrl);
+
+        if (!this.state.playing) {
+          audio.play();
+          this.setState({ playing: true, audio, playingPreviewUrl: previewUrl })
+        } else {
+          this.state.audio.pause();
+
+          if (this.state.playingPreviewUrl === previewUrl) {
+            this.setState({ playing: false });
+          } else {
+            audio.play();
+            this.setState({ audio, playingPreviewUrl: previewUrl })
+          }
+        }
+    }
+
     render () {
         const { tracks } = this.props
 
@@ -12,8 +32,9 @@ class Tracks extends Component {
 
                         return (
                             <div
-                                key={id}
-                                className='track'
+                                 key={id}
+                                 onClick={this.playAudio(preview_url)}
+                                 className='track'
                                 >
                                 <img
                                   src={album.images[0].url}
