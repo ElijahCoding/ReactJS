a554cde03a9058eefa2867f07735040b08638d2a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-03 14:21:20 +0300

    set up babel

--- Basic/public/scripts/app.js
+++ Basic/public/scripts/app.js
@@ -1,12 +1,6 @@
 console.log('App.js is running!');
 
-// var template = <p>This is JSX from app.js!</p>;
-var template = React.createElement(
-  "h1",
-  { id: "someid" },
-  "Something new"
-);
-
+var template = <p>This is JSX from app.js!</p>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
