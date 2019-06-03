f3f58271335151af6c7520d16aa792d917a7599a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-03 15:35:48 +0300

    passing props

--- ReactLearning/public/app.jsx
+++ ReactLearning/public/app.jsx
@@ -1,4 +1,16 @@
+var Greeter = React.createClass({
+    render: function () {
+        var name = this.props.name
+        return (
+            <div>
+                <h1>Hello {name} !</h1>
+                <p>This is form a component!</p>
+            </div>
+        );
+    }
+})
+
 ReactDOM.render(
-    <h1>hello react</h1>,
+    <Greeter name="elijah" />,
     document.getElementById('app')
 );
