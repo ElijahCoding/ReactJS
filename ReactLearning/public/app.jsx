2a823a85b1a00441030c7472dadb4c2e9ca83615
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-03 15:59:04 +0300

    onSubmit event

--- ReactLearning/public/app.jsx
+++ ReactLearning/public/app.jsx
@@ -3,12 +3,24 @@ var Greeter = React.createClass({
         return {}
     },
 
+    onButtonClick: function (e) {
+        e.preventDefault();
+
+        var name = this.refs.name.value;
+        alert(name);
+    },
+
     render: function () {
         var name = this.props.name
         return (
             <div>
                 <h1>Hello {name} !</h1>
                 <p>This is form a component!</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         );
     }
