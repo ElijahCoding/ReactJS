93f4d89f08fedfb890de8572eb6e6d3c70b1eae8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-04 17:52:28 +0300

    nav built

--- Timer/app/components/Main.jsx
+++ Timer/app/components/Main.jsx
@@ -1,12 +1,14 @@
 var React = require('react');
+var Navigation = require('Navigation');
 
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
-          <p>Main.jsx Rendered</p>
-          {props.children}
+            <Navigation />
+            <p>Main.jsx Rendered</p>
+            {props.children}
         </div>
       </div>
     </div>
