b0a71cdc45549b7e23671a634c9e4d7ce304fc37
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-06 20:18:42 +0300

    input form

--- Timer/app/components/Main.jsx
+++ Timer/app/components/Main.jsx
@@ -2,17 +2,16 @@ var React = require('react');
 var Navigation = require('Navigation');
 
 var Main = (props) => {
-  return (
-    <div>
+    return (
       <div>
-        <div>
-            <Navigation />
-            <p>Main.jsx Rendered</p>
+        <Navigation/>
+        <div className="row">
+          <div className="column small-centered medium-6 large-4">
             {props.children}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 module.exports = Main;
