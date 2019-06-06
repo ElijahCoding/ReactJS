77323bd55c47d650bc6bf13859833c8cc3360f0e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-06 19:44:00 +0300

    rendering

--- Timer/app/tests/components/Clock.test.jsx
+++ Timer/app/tests/components/Clock.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDom = require('react-dom');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
@@ -8,6 +8,16 @@ var Clock = require('Clock');
 describe('Clock', () => {
     it('should exist', () => {
         expect(Clock).toExist();
+    });
+
+    describe('render', () => {
+        it('should render clock to output', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+        });
     });
 
     describe('formatSeconds', () => {
