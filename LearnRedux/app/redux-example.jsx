ab186a742b7472fe6cec5b9864068e308d21401d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-12 14:54:08 +0300

    action dispatching

--- LearnRedux/app/redux-example.jsx
+++ LearnRedux/app/redux-example.jsx
@@ -1,12 +1,26 @@
 var redux = require('redux');
 
-console.log('Starting redux example');
-
 var reducer = (state = { name: 'anonymous' }, action) => {
-    return state;
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            }
+        default:
+            return state;
+    }
 }
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState:', currentState);
+
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Elijah'
+});
+
+console.log('Name should be Elijah:', store.getState());
