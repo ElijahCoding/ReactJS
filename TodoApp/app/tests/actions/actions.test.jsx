b1b863191b6140982d4c04692d7c328e84bff303
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-13 18:06:20 +0300

    building action generators

--- TodoApp/app/tests/actions/actions.test.jsx
+++ TodoApp/app/tests/actions/actions.test.jsx
@@ -1,2 +1,25 @@
 var expect = require('expect');
 var actions = require('actions');
+
+describe('Actions', () => {
+    it('should generate search text action', () => {
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'test text'
+        };
+
+        var res = actions.setSearchText(action.searchText);
+        expect(res).toEqual(action);
+    });
+
+    it('should generate add todo action', () => {
+        var action = {
+            type: 'ADD_TODO',
+            text: 'todo text'
+        };
+
+        var res = actions.addTodo(action.text);
+
+        expect(res).toEqual(action);
+    });
+});
